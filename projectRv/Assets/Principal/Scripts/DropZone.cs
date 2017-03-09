b73b7a37ad133716aa15ae8e5b0c225d804bcad1
Herminio Paucar <hpc424@gmail.com>
2017-03-08 22:01:50 -0300

    new version of drag and drop

---
+++ projectRv/Assets/Principal/Scripts/DropZone.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.EventSystems;
+
+public class DropZone : MonoBehaviour, IDropHandler, IPointerEnterHandler, IPointerExitHandler {
+
+	public void OnPointerEnter(PointerEventData eventData) {
+		//Debug.Log("OnPointerEnter");
+		if(eventData.pointerDrag == null)
+			return;
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d != null) {
+			d.placeholderParent = this.transform;
+		}
+	}
+	
+	public void OnPointerExit(PointerEventData eventData) {
+		//Debug.Log("OnPointerExit");
+		if(eventData.pointerDrag == null)
+			return;
+
+		Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+		if(d != null && d.placeholderParent==this.transform) {
+			d.placeholderParent = d.parentToReturnTo;
+		}
+	}
+	
+	public void OnDrop(PointerEventData eventData) {
+
+        //if (img.rect.Contains(img.InverseTransformPoint(Input.mousePosition)){ }
+        Debug.Log (eventData.pointerDrag.name + " was dropped on " + gameObject.name + " Mouse Position " + Input.mousePosition);
+
+        // eventData.pointerDrag: represent the object in movement.
+        Debug.Log(eventData.pointerDrag.transform.position + "   :) ");
+
+        //gameObject: represent to panel.
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+
+        /*
+        print(" xMax: "+ gameObject.GetComponent<RectTransform>().rect.xMax + "xMin: "+
+            gameObject.GetComponent<RectTransform>().rect.xMin + "yMax: " +
+            gameObject.GetComponent<RectTransform>().rect.yMax + "yMin: " +
+            gameObject.GetComponent<RectTransform>().rect.yMin);
+        */
+
+        //print(" Height Panel:   " + gameObject.GetComponent<RectTransform>().rect.height*0.374769);
+        print(" MeshRenderer Panel:   " + gameObject.GetComponent<MeshFilter>().mesh.bounds);
+
+
+
+    }
+}
